8a1c19661ba6798354b6dc0c3008f076493d2131
Rafael Sobral <sobralrafa.mwr@gmail.com>
2023-11-30 18:48:17 -0300

    Primeiro commit

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const {VueLoaderPlugin} = require('vue-loader');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+modelo.exports = {
+    entry: './src/main.js'
+}
